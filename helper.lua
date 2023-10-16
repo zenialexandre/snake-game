5fb6096b41645466f55edf39da9e822bac7735d4
alexandre-zeni <alexandre.zeni@philips.com>
2023-10-16 11:55:11 -0300

    adding game state handling

--- helper.lua
+++ helper.lua
@@ -1,35 +1,76 @@
 local love = require "love"
 
+function change_game_state(game_properties, new_state)
+    game_properties.state["starting_point"] = new_state == "starting_point"
+    game_properties.state["paused"] = new_state == "paused"
+    game_properties.state["running"] = new_state == "running"
+    game_properties.state["ended"] = new_state == "ended"
+end
+
 function handle_movement_by_keyboard(in_game_snake, movement_direction)
     if movement_direction == "right" then
-        in_game_snake.position_x = in_game_snake.position_x + 0.5
+        in_game_snake.position_x = in_game_snake.position_x + 5
     elseif movement_direction == "left" then
-        in_game_snake.position_x = in_game_snake.position_x - 0.5
+        in_game_snake.position_x = in_game_snake.position_x - 5
     elseif movement_direction == "up" then
-        in_game_snake.position_y = in_game_snake.position_y - 0.5
+        in_game_snake.position_y = in_game_snake.position_y - 5
     elseif movement_direction == "down" then
-        in_game_snake.position_y = in_game_snake.position_y + 0.5
+        in_game_snake.position_y = in_game_snake.position_y + 5
     end
 end
 
-function check_border_collision(in_game_snake, movement_direction)
+function handle_game_replay(game_properties, in_game_snake, in_game_fruit)
+    if game_properties.state["ended"] then
+        game_properties.fruits_eaten = 0
+        in_game_snake.position_x = 380
+        in_game_snake.position_y = 280
+        in_game_fruit.position_x = math.random(1, love.graphics.getWidth() / 1.25)
+        in_game_fruit.position_y = math.random(1, love.graphics.getWidth() / 1.25)
+        change_game_state(game_properties, "running")
+    else
+        change_game_state(game_properties, "running")
+    end
+end
+
+function handle_game_pause(game_properties)
+    if game_properties.state["running"] then
+        change_game_state(game_properties, "paused")
+    end
+end
+
+function check_border_collision(game_properties, in_game_snake, movement_direction)
     if movement_direction == "right" and in_game_snake.position_x == love.graphics.getWidth() - in_game_snake.width then
-        love.window.close()
+        change_game_state(game_properties, "ended")
     elseif movement_direction == "left" and in_game_snake.position_x == 0 then
-        love.window.close()
+        change_game_state(game_properties, "ended")
     elseif movement_direction == "down" and in_game_snake.position_y == love.graphics.getHeight() - in_game_snake.height then
-        love.window.close()
+        change_game_state(game_properties, "ended")
     elseif movement_direction == "up" and in_game_snake.position_y == 0 then
-        love.window.close()
+        change_game_state(game_properties, "ended")
     end
 end
 
-function check_eaten_fruit(in_game_snake, in_game_fruit)
+function check_eaten_fruit(in_game_snake, in_game_fruit, game_properties)
     if (in_game_snake.position_y + in_game_snake.height > in_game_fruit.position_y)
         and (in_game_snake.position_y < in_game_fruit.position_y + in_game_fruit.height)
         and (in_game_snake.position_x + in_game_snake.width > in_game_fruit.position_x)
         and (in_game_snake.position_x < in_game_fruit.position_x + in_game_fruit.width) then
         in_game_fruit.position_x = math.random(1, love.graphics.getWidth() / 1.25)
         in_game_fruit.position_y = math.random(1, love.graphics.getHeight() / 1.25)
+        game_properties.fruits_eaten = game_properties.fruits_eaten + 1
+    end
+end
+
+function check_game_state(game_properties, fonts)
+    if game_properties.state["starting_point"] then
+        love.graphics.printf("Press a movement key to begin.", fonts.large.font, 0, 50, love.graphics.getWidth(), "center")
+    elseif game_properties.state["paused"] then
+        love.graphics.printf("Paused, press a movement key to continue.", fonts.large.font, 0, 50, love.graphics.getWidth(), "center")
+        love.graphics.printf("Fruits eaten: " .. game_properties.fruits_eaten, fonts.medium.font, 0, 90, love.graphics.getWidth(), "center")
+    elseif game_properties.state["ended"] then
+        love.graphics.setFont(fonts.large.font)
+        love.graphics.printf("Game Ended! :(", fonts.large.font, 0, 50, love.graphics.getWidth(), "center")
+        love.graphics.printf("You eated " .. game_properties.fruits_eaten .. " fruits.", fonts.medium.font, 0, 90, love.graphics.getWidth(), "center")
+        love.graphics.printf("Press any movement key to replay it.", fonts.medium.font, 0, 120, love.graphics.getWidth(), "center")
     end
 end
--- main.lua
+++ main.lua
@@ -2,41 +2,65 @@ math.randomseed(os.time())
 
 _G.love = require "love"
 require "helper"
+
 local snake = require "Snake"
 local fruit = require "Fruit"
-
-local game = {
- state = {
-  menu = true,
-  paused = false,
-  running = false,
-  ended = false
- }
-}
-
 local in_game_snake = snake()
 local in_game_fruit = fruit()
 local movement_direction = ""
+
+local game_properties = {
+    state = {
+        starting_point = true,
+        paused = false,
+        running = false,
+        ended = false
+    },
+    fruits_eaten = 0
+}
+
+local fonts = {
+    medium = {
+        font = love.graphics.newFont(16),
+        size = 16
+    },
+    large = {
+        font = love.graphics.newFont(24),
+        size = 24
+    },
+    massive = {
+        font = love.graphics.newFont(60),
+        size = 60
+    }
+}
 
 function love.load()
     love.mouse.setVisible(false)
 end
 
 function love.update(dt)
-    handle_movement_by_keyboard(in_game_snake, movement_direction)
-    check_border_collision(in_game_snake, movement_direction)
-    check_eaten_fruit(in_game_snake, in_game_fruit)
+    if game_properties.state["running"] then
+        handle_movement_by_keyboard(in_game_snake, movement_direction)
+        check_border_collision(game_properties, in_game_snake, movement_direction)
+        check_eaten_fruit(in_game_snake, in_game_fruit, game_properties)
+    end
 end
 
 function love.keypressed(key)
     if key == "d" or key == "right" then
         movement_direction = "right"
+        handle_game_replay(game_properties, in_game_snake, in_game_fruit)
     elseif key == "a" or key == "left" then
         movement_direction = "left"
+        handle_game_replay(game_properties, in_game_snake, in_game_fruit)
     elseif key == "w" or key == "up" then
         movement_direction = "up"
+        handle_game_replay(game_properties, in_game_snake, in_game_fruit)
     elseif key == "s" or key == "down" then
         movement_direction = "down"
+        handle_game_replay(game_properties, in_game_snake, in_game_fruit)
+    elseif key == "escape" then
+        handle_game_pause(game_properties)
     end
 end
 
@@ -46,4 +70,6 @@ function love.draw()
 
     love.graphics.setColor(1, 0, 0)
     love.graphics.rectangle("fill", in_game_fruit.position_x, in_game_fruit.position_y, in_game_fruit.width, in_game_fruit.height)
+
+    check_game_state(game_properties, fonts)
 end
